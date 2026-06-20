eca6f124ee1666c523f4612c1a7b6fd14373a835
RenanBrigagao <renan.brigagao@gmailcom>
2026-06-20 19:32:29 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame
+
+print('Setup Start')
+pygame.init()
+screen = pygame.display.set_mode(size=(1000, 800))
+
+while True:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit() #Close Screen
+            quit() #End
