a86545297e9d187dd68af86129067e1d52c2191b
RenanBrigagao <renan.brigagao@gmailcom>
2026-06-23 00:21:52 -0300

    Detalhes

--- Code/background.py
+++ Code/background.py
@@ -3,12 +3,12 @@
 from Code.const import WIN_WIDTH, ENTITY_SPEED
 from Code.entity import Entity
 
+
 class Background(Entity):
     def __init__(self, name: str, position: tuple):
         super().__init__(name, position)
 
-
-    def move(self, ):
+    def move(self):
         self.rect.centerx -= ENTITY_SPEED[self.name]
         if self.rect.right <= 0:
             self.rect.left = WIN_WIDTH
